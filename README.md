ff8bef842f07ffaa67c980d20a9b278e9da67850
Christian Zangl <laktak@cdak.net>
2019-07-22 14:57:40 +0200

    initial

---
+++ README.md
@@ -0,0 +1,21 @@
+# pipenv-shebang
+
+*pipenv-shebang* allows you to put scripts in your path that run in a pipenv environment.
+
+## Usage
+
+Put this shebang at the top of your script:
+
+```
+#!/bin/env pipenv-shebang
+```
+
+## Installation
+
+Supports Python 3.6 on Linux or macOS.
+
+```
+sudo pip install pipenv-shebang
+# or
+pip install --user pipenv-shebang
+```
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="pipenv-shebang",
+    version="0.0.1",
+    url="https://github.com/laktak/pipenv-shebang",
+    author="Christian Zangl",
+    author_email="laktak@cdak.net",
+    description="pipenv-shebang allows you to put scripts in your path that run in a pipenv environment.",
+    packages=[],
+    install_requires=[],
+    scripts=["pipenv-shebang"],
+)
